8e53805703ada7474723a0a16602c68799276117
haelilac <shanacarmellabolok@gmail.com>
2025-03-25 10:41:57 +0800

    cors update

--- config/cors.php
+++ config/cors.php
@@ -2,23 +2,23 @@
 
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', '/login-admin-tenant', '/logout'],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'https://seagold-dormitory.vercel.app', 
-        'http://localhost:3000'
+        'https://seagold-dormitory.vercel.app',
+        'http://localhost:3000',
+        'http://127.0.0.1:3000'
     ],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Content-Type', 'X-Requested-With', 'X-CSRF-TOKEN', 'Authorization', 'Accept'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['*'],
 
     'max_age' => 0,
 
     'supports_credentials' => true,
 ];
-
