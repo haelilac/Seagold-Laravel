1118cc6bb1e218388ff529c0c201c2b93b84ba40
haelilac <shanacarmellabolok@gmail.com>
2025-03-28 10:25:22 +0800

    Update PaymentController.php

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -273,7 +273,7 @@ public function updateStatus($user_id)
             try {
                 // 🔍 Prepare the file to be sent to FastAPI
                 $client = new \GuzzleHttp\Client();
-                $response = $client->post('https://seagold-python-production.up.railway.app/upload-id/', [
+                $response = $client->post('https://seagold-laravel-production.up.railway.app/api/upload-id/', [
                     'multipart' => [
                         [
                             'name' => 'file',
