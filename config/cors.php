2004071804e148679bd58954d944ac602e33af5f
haelilac <shanacarmellabolok@gmail.com>
2025-04-04 10:42:59 +0800

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -1,23 +1,15 @@
 <?php
 
-
 return [
-        'paths' => ['api/*', 'sanctum/csrf-cookie', '/login-admin-tenant', '/logout', 'upload-id'],
-        'allowed_methods' => ['*'],
-
-        'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization', 'Accept'],
-
-        
-        'allowed_origins' => [
-            'https://seagold-dormitory.vercel.app',
-            'https://seagold-python-production.up.railway.app',
-        ],
-
-        'allowed_origins_patterns' => [],
-
-        'exposed_headers' => ['Authorization', 'X-CSRF-TOKEN'],
-
-        'max_age' => 0,
+    'paths' => ['api/*', 'upload-id', 'sanctum/csrf-cookie', '/login-admin-tenant', '/logout'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        'https://seagold-dormitory.vercel.app',
+    ],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization', 'Accept'],
+    'exposed_headers' => ['Authorization', 'X-CSRF-TOKEN'],
+    'max_age' => 0,
 
         'supports_credentials' => true,
 ];
