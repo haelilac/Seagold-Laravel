c94de95676a3108b3f49ca0b65ac1893c07dbacf
haelilac <shanacarmellabolok@gmail.com>
2025-04-04 11:32:01 +0800

    removed onrender

--- config/cors.php
+++ config/cors.php
@@ -2,50 +2,21 @@
 
 
 return [
-        'paths' => ['api/*', 'sanctum/csrf-cookie', '/login-admin-tenant', '/logout', 'upload-id'],
-        'allowed_methods' => ['*'],
-
-        'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization', 'Accept'],
-
-        
-        'allowed_origins' => [
-            'https://seagold-dormitory.vercel.app',
-        ],
-
-        'allowed_origins_patterns' => [],
-
-        'exposed_headers' => ['Authorization', 'X-CSRF-TOKEN'],
-
-        'max_age' => 0,
-
-        'supports_credentials' => true,
-];
-
-
-
-/*
-return [
-
-    'paths' => ['api/*', 'sanctum/csrf-cookie', '/login-admin-tenant', '/logout'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login-admin-tenant', 'logout', 'upload-id'],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
         'https://seagold-dormitory.vercel.app',
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        'https://seagold-laravel-production.up.railway.app'
     ],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['Content-Type', 'X-Requested-With', 'X-CSRF-TOKEN', 'Authorization', 'Accept'],
+    'allowed_headers' => ['*'],
 
-    'exposed_headers' => ['*'],
+    'exposed_headers' => ['Authorization', 'X-CSRF-TOKEN'],
 
     'max_age' => 0,
 
     'supports_credentials' => true,
 ];
-
-*/
