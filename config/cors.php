0fda40da3aefe2eff6c4201d3a0df6be378beed7
haelilac <shanacarmellabolok@gmail.com>
2025-04-27 16:02:55 +0800

    Deployment new domain

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@
 return [
 'paths' => ['api/*', 'sanctum/csrf-cookie', 'login-admin-tenant', 'logout', 'upload-id'],
 'allowed_methods' => ['*'],
-'allowed_origins' => ['https://seagold-dormitory.vercel.app'],
+'allowed_origins' => ['https://seagold-dormitory.com'],
 'allowed_origins_patterns' => [],
 'allowed_headers' => ['*'],
 'exposed_headers' => ['Authorization', 'X-CSRF-TOKEN'],
