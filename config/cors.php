d022b4ab1c1c866a5c90a92be3e85ea071795009
haelilac <shanacarmellabolok@gmail.com>
2025-04-29 11:38:35 +0800

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -5,8 +5,10 @@ return [
 'paths' => ['api/*', 'sanctum/csrf-cookie', 'login-admin-tenant', 'logout', 'upload-id'],
 'allowed_methods' => ['*'],
 'allowed_origins' => [
+    'http://localhost:3000',
     'https://seagold-dormitory.com',
     'https://www.seagold-dormitory.com',
+    'https://seagold-laravel-production.up.railway.app'
 ],
 'allowed_origins_patterns' => [],
 'allowed_headers' => ['*'],
