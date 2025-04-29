50bb7217cc04dfe8351bbc2183c8952aa85eb048
haelilac <shanacarmellabolok@gmail.com>
2025-04-29 09:32:39 +0800

    Update ApplicationController.php

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Facades\Hash;
 use App\Models\Application;
 use App\Models\User;
 use App\Models\Unit;
-use Kreait\Firebase\Factory;
 use App\Services\FirebaseService;
 use App\Events\NewApplicationSubmitted;
 use App\Events\NewAdminNotificationEvent;
@@ -106,33 +105,6 @@ public function unitsOnly()
         }
     }
     
-    public function verifyGoogleToken(Request $request, FirebaseService $firebaseService)
-    {
-        // Validate incoming data
-        $validated = $request->validate([
-            'token' => 'required|string',
-            'provider' => 'required|string',
-        ]);
-    
-        // Validate token with Firebase
-        try {
-            $auth = $firebaseService->auth();
-            $verifiedIdToken = $auth->verifyIdToken($validated['token']);
-            $uid = $verifiedIdToken->getClaim('sub');
-    
-            // Get user information (for example email and name)
-            $user = User::where('firebase_uid', $uid)->first();
-            
-            if ($user) {
-                return response()->json(['email' => $user->email, 'name' => $user->name]);
-            } else {
-                return response()->json(['message' => 'User not found'], 404);
-            }
-        } catch (\Exception $e) {
-            \Log::error('Google Token Verify Error', ['error' => $e->getMessage()]);
-            return response()->json(['message' => 'Failed to verify token', 'error' => $e->getMessage()], 400);
-        }
-    }
     
     // Save a new application
     public function store(Request $request)
