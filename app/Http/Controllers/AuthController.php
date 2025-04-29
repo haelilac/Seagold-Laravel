428d664deb9155077066d9a7b62b77f3dacc1dac
haelilac <shanacarmellabolok@gmail.com>
2025-04-29 19:33:14 +0800

    sssss

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,8 +16,21 @@ class AuthController extends Controller
 
     private function createFirebaseAuth()
     {
+        $serviceAccount = [
+            'type' => env('FIREBASE_TYPE'),
+            'project_id' => env('FIREBASE_PROJECT_ID'),
+            'private_key_id' => env('FIREBASE_PRIVATE_KEY_ID'),
+            'private_key' => str_replace("\\n", "\n", env('FIREBASE_PRIVATE_KEY')),
+            'client_email' => env('FIREBASE_CLIENT_EMAIL'),
+            'client_id' => env('FIREBASE_CLIENT_ID'),
+            'auth_uri' => env('FIREBASE_AUTH_URI'),
+            'token_uri' => env('FIREBASE_TOKEN_URI'),
+            'auth_provider_x509_cert_url' => env('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
+            'client_x509_cert_url' => env('FIREBASE_CLIENT_X509_CERT_URL'),
+        ];
+    
         return (new Factory)
-            ->withServiceAccount(storage_path('app/firebase-service-account.json')) // 👈 your JSON path
+            ->withServiceAccount($serviceAccount)
             ->createAuth();
     }
     
