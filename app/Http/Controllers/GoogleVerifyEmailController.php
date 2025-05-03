000255c597bd6b3a797fdbb2e44f9782ab20b62f
haelilac <shanacarmellabolok@gmail.com>
2025-05-03 17:41:02 +0800

    Update GoogleVerifyEmailController.php

--- app/Http/Controllers/GoogleVerifyEmailController.php
+++ app/Http/Controllers/GoogleVerifyEmailController.php
@@ -21,9 +21,19 @@ class GoogleVerifyEmailController extends Controller
                 throw new \Exception('Service account file is missing.');
             }
 
-            $firebase = (new Factory)
-                ->withServiceAccount(storage_path('app/firebase-service-account.json'));
-
+            $firebase = (new Factory)->withServiceAccount([
+                'type' => env('FIREBASE_TYPE'),
+                'project_id' => env('FIREBASE_PROJECT_ID'),
+                'private_key_id' => env('FIREBASE_PRIVATE_KEY_ID'),
+                'private_key' => str_replace("\\n", "\n", env('FIREBASE_PRIVATE_KEY')),
+                'client_email' => env('FIREBASE_CLIENT_EMAIL'),
+                'client_id' => env('FIREBASE_CLIENT_ID'),
+                'auth_uri' => env('FIREBASE_AUTH_URI'),
+                'token_uri' => env('FIREBASE_TOKEN_URI'),
+                'auth_provider_x509_cert_url' => env('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
+                'client_x509_cert_url' => env('FIREBASE_CLIENT_X509_CERT_URL'),
+            ]);
+            
             $this->auth = $firebase->createAuth();
 
             Log::info('[Firebase] Firebase Auth initialized successfully.');
