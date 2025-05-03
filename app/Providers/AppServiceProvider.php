d059fd066771f697414d227ab0b98ce2a1612288
haelilac <shanacarmellabolok@gmail.com>
2025-05-03 18:57:25 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,25 +5,30 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\Auth;
-use Kreait\Firebase\ServiceAccount;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->singleton(\Kreait\Firebase\Auth::class, function ($app) {
-            $credentials = config('firebase');
-            
-            $factory = (new Factory)
-                ->withServiceAccount(ServiceAccount::fromValue($credentials))
-                ->withProjectId($credentials['project_id']);
+        $this->app->singleton(Auth::class, function ($app) {
+            $firebaseCredentials = [
+                "type" => env('FIREBASE_TYPE'),
+                "project_id" => env('FIREBASE_PROJECT_ID'),
+                "private_key_id" => env('FIREBASE_PRIVATE_KEY_ID'),
+                "private_key" => str_replace("\\n", "\n", env('FIREBASE_PRIVATE_KEY')),
+                "client_email" => env('FIREBASE_CLIENT_EMAIL'),
+                "client_id" => env('FIREBASE_CLIENT_ID'),
+                "auth_uri" => env('FIREBASE_AUTH_URI'),
+                "token_uri" => env('FIREBASE_TOKEN_URI'),
+                "auth_provider_x509_cert_url" => env('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
+                "client_x509_cert_url" => env('FIREBASE_CLIENT_X509_CERT_URL'),
+            ];
 
-            return $factory->createAuth();
+            return (new Factory)
+                ->withServiceAccount($firebaseCredentials)
+                ->createAuth();
         });
     }
 
-    public function boot()
-    {
-        //
-    }
+    public function boot() {}
 }
