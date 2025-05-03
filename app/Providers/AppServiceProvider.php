5d4df6602531520403ac77fb128ee3cef0a53b9c
haelilac <shanacarmellabolok@gmail.com>
2025-05-03 19:15:16 +0800

    aaaaa

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,27 +5,19 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\Auth;
+use Illuminate\Support\Facades\File;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->singleton(Auth::class, function ($app) {
-            $firebaseCredentials = [
-                "type" => env('FIREBASE_TYPE'),
-                "project_id" => env('FIREBASE_PROJECT_ID'),
-                "private_key_id" => env('FIREBASE_PRIVATE_KEY_ID'),
-                "private_key" => str_replace("\\n", "\n", env('FIREBASE_PRIVATE_KEY')),
-                "client_email" => env('FIREBASE_CLIENT_EMAIL'),
-                "client_id" => env('FIREBASE_CLIENT_ID'),
-                "auth_uri" => env('FIREBASE_AUTH_URI'),
-                "token_uri" => env('FIREBASE_TOKEN_URI'),
-                "auth_provider_x509_cert_url" => env('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
-                "client_x509_cert_url" => env('FIREBASE_CLIENT_X509_CERT_URL'),
-            ];
-
+        $this->app->singleton(Auth::class, function () {
+            $json = env('FIREBASE_CREDENTIALS_JSON');
+            $tmpJsonPath = storage_path('app/firebase_tmp.json');
+            file_put_contents($tmpJsonPath, $json);
+    
             return (new Factory)
-                ->withServiceAccount($firebaseCredentials)
+                ->withServiceAccount($tmpJsonPath)
                 ->createAuth();
         });
     }
