22ec40c47150b283a3a66c0930adaf0e9f39ced7
haelilac <shanacarmellabolok@gmail.com>
2025-05-03 18:51:43 +0800

    sssss

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,16 +5,19 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\Auth;
+use Kreait\Firebase\ServiceAccount;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register()
     {
         $this->app->singleton(\Kreait\Firebase\Auth::class, function ($app) {
+            $credentials = config('firebase');
+            
             $factory = (new Factory)
-                ->withServiceAccount(config('firebase'))
-                ->withProjectId(env('FIREBASE_PROJECT_ID'));
-    
+                ->withServiceAccount(ServiceAccount::fromValue($credentials))
+                ->withProjectId($credentials['project_id']);
+
             return $factory->createAuth();
         });
     }
